1623be6aa900f530bcfdddb0d1d1290c78270fd6
Kim Hyeon Ji <rlaguswl_01@naver.com>
2025-09-03 15:14:32 +0900

    update

---
+++ edu/src/edu/Test1.java
@@ -0,0 +1,9 @@
+package edu;
+
+public class Test1{
+	public static void main(String[] args) {
+	System.out.println("Hello, 안녕!");
+	}
+}
+
+// public static void main(String[] args) { } 구조는 main 적고 ctrl + space
