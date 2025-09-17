6f6aea769cccfea1f18b2ca582b5f0c30fc1ffb2
Kim Hyeon Ji <rlaguswl_01@naver.com>
2025-09-17 14:36:57 +0900

    리드미 임시저장

--- edu/src/com/ruby/java/ch02데이터타입/Test02.java
+++ edu/src/com/ruby/java/ch02데이터타입/Test02.java
@@ -1,8 +1,8 @@
-package edu;
+package com.ruby.java.ch02데이터타입;
 
 public class Test02 {
 	public static void main(String[] args) {
-		int depositAmount;
+		int depositAmount; // int는 4byte
 		depositAmount = 50000;
 		System.out.println(depositAmount);
 	}
